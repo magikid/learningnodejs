324ef29df1eb5514d2fc31190f10e8f9e7fda185
Chris Jones <chris@christopherjones.us>
2012-09-12 06:28:47 -0400

    Just kidding, really fixed merge this time.

--- router.js
+++ router.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 function route(pathname){
     console.log("About to route " + pathname);
 }
 
-=======
-function route(pathname){
-    console.log("About to route " + pathname);
-}
-
->>>>>>> d301e2926ebc5d41b678d79fb319d7edac55bf7c
 exports.route = route;
