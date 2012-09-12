7c6aee78cd8a049fee4e31c29659e64a26f2c036
Chris Jones <chris@christopherjones.us>
2012-09-12 07:33:42 -0400

    Installed formidable and documented where I left off.

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 
 server.start(router.route, handle);
+
+//falling asleep, leaving off right before doing anything with formidable.
