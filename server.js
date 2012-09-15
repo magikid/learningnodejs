af12b9aacce71b5297f8d265c8d529bc88c316d2
Chris Jones <chris@christopherjones.us>
2012-09-14 22:38:42 -0400

    completed project

--- server.js
+++ server.js
@@ -3,24 +3,12 @@ var url = require("url");
 
 function start(route, handle){
     function onRequest(request, response) {
-        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received");
-        
-        request.setEncoding("utf8");
-        
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("Got POST data chunk '"+ postDataChunk + "'.");
-        });
-        
-        request.addListener("end", function(){
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, request);
     }
     
     http.createServer(onRequest).listen(process.env.PORT);
-    
     console.log("Server started");
 }
 
