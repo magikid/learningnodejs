2b10c2a8ad43d6fedf3ff7d7d536c87bcb530d7c
Chris Jones <chris@christopherjones.us>
2013-02-02 20:47:15 -0500

    Made it through first bit of express tut, using express to generate site now

--- secondattempt/express.js
+++ secondattempt/express.js
@@ -3,3 +3,13 @@
  * Following the express quick start located here: http://expressjs.com/guide.html
  * 
  *********************************/
+ 
+var express = require('express');
+var app = express();
+
+app.get('/hello.txt', function(req, res){
+        res.send('Hello world');
+    })
+    
+app.listen(process.env.PORT);
+console.log('Listening on port ' + process.env.PORT);
