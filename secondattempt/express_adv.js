abfbb8962abafcd345665c6913bad1f784b921b6
Chris Jones <chris@christopherjones.us>
2013-02-02 21:19:31 -0500

    Got rid of express template dir.  Started more advanced express tutorial.

---
+++ secondattempt/express_adv.js
@@ -0,0 +1,7 @@
+/*
+ * Following these tutorials some:
+ * http://bengourley.co.uk/using-stylus
+ * http://clock.co.uk/tech-blogs/a-simple-website-in-nodejs-with-express-jade-and-stylus
+ * http://shapeshed.com/creating-a-basic-site-with-node-and-express/
+ *
+ */
